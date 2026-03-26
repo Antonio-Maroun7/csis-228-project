0f5779973a411ff4be6acc5ad654878fdd338a77
Antonio Maroun <antonio@gmail.com>
2026-03-26 17:41:05 +0200

    finished implementing staff_services entity

--- src/entities/staff_services.entity.js
+++ src/entities/staff_services.entity.js
@@ -3,5 +3,26 @@
  * Uses DB column names (snake_case).
  */
 
-class Staff_ServicesEntity {}
+const { fromRow } = require("./user.entity");
+
+class Staff_ServicesEntity {
+  constructor({
+    staff_id,
+    service_id,
+    staff_duration_min,
+    staff_price_cents,
+  } = {}) {
+    this.staff_id = staff_id;
+    this.service_id = service_id;
+    this.staff_duration_min = staff_duration_min;
+    this.staff_price_cents = staff_price_cents;
+  }
+  static fromRow(row) {
+    if (!row) return null;
+    return new Staff_ServicesEntity(row);
+  }
+  static fromRows(rows) {
+    return (rows || []).map((row) => Staff_ServicesEntity.fromRow(row));
+  }
+}
 module.exports = Staff_ServicesEntity;
