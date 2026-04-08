a86c3af66577a948495826b539ec1bb7867a4b41
Antonio Maroun <antonio@gmail.com>
2026-04-09 02:09:39 +0300

    AuthController implemented

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+const AuthService = require("../services/auth.service");
+const { handleError } = require("../utils/errorHandler");
+
+class AuthController {
+  static async login(req, res) {
+    try {
+      const result = await AuthService.login(req.body);
+      res.status(200).json(result);
+    } catch (err) {
+      return handleError(res, err);
+    }
+  }
+}
+module.exports = AuthController;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,24 +4,29 @@ const UserDto = require("../dto/user.dto");
 
 class AuthService {
   static async login({ email, password }) {
-    const expectedUser = await AuthRepository.authenticate(email, password);
-    if (!expectedUser) {
-      throw new Error("Invalid Credentials");
-    }
-    if (!expectedUser.user_is_active) {
-      throw new Error("user is not active");
-    }
-    const token = generateToken({
-      sub: expectedUser.user_id,
-      email: expectedUser.user_email,
-      role: expectedUser.user_role,
-    });
+    try {
+      const expectedUser = await AuthRepository.authenticate(email, password);
+      if (!expectedUser) {
+        throw new Error("Invalid Credentials");
+      }
+      if (!expectedUser.user_is_active) {
+        throw new Error("user is not active");
+      }
+      const token = generateToken({
+        sub: expectedUser.user_id,
+        email: expectedUser.user_email,
+        role: expectedUser.user_role,
+      });
 
-    return {
-      message: "login successful",
-      token,
-      data: UserDto.toResponseDto(expectedUser),
-    };
+      return {
+        message: "login successful",
+        token,
+        data: UserDto.toResponseDto(expectedUser),
+      };
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    }
   }
 }
 module.exports = AuthService;
