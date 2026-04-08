e906673554fee03c09acabd40678177c94b22946
Antonio Maroun <antonio@gmail.com>
2026-04-08 18:37:20 +0300

    appointmentDto created

---
+++ src/dto/appointment_item.dto.js
@@ -0,0 +1,9 @@
+/**
+ * AppointmentItems DTOs – API contract (camelCase response/request shapes).
+ * All entity ↔ DTO conversion is done in the appointmentItem mapper.
+ */
+const AppointmentItemMapper = require("../mappers/appointment_item.mapper");
+module.exports = {
+  toResponseDto: AppointmentItemMapper.entityToResponseDto,
+  toListDto: AppointmentItemMapper.entityToListDto,
+};
