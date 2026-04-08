ce46d3d4957c7750501f4e6733be9177abf9d0f0
Antonio Maroun <antonio@gmail.com>
2026-04-09 02:15:36 +0300

    AuthRoute implemented

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const serviceRoute = require("./routes/services.route");
 const CategoryRoute = require("./routes/category.route");
 const AppointmentRoute = require("./routes/appointment.route");
 const AppointmentItemRoute = require("./routes/appointment_item.route");
+const AuthRoute = require("./routes/auth.routes");
 
 const app = express();
 app.use(express.json());
 
+app.use("/api/auth", AuthRoute);
 app.use("/api/appointmentItems", AppointmentItemRoute);
 app.use("/api/appointments", AppointmentRoute);
 app.use("/api/categories", CategoryRoute);
