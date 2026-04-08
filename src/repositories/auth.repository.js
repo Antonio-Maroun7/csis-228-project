085f2df9263a859eb0f47472ff6f1158645c0dd3
Antonio Maroun <antonio@gmail.com>
2026-04-09 01:38:24 +0300

    AuthRepository created

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,1 +1,16 @@
+const pool = require("../db/pool");
+const UserEntity = require("../entities/user.entity");
 
+class AuthRepository {
+  static async authenticate(email, password) {
+    const q = `
+        SELECT *
+        FROM users 
+        WHERE user_email =$1
+        AND user_password=$2`;
+    const params = [email, password];
+    const { rows } = await pool.query(q, params);
+    return UserEntity.fromRow(rows[0]);
+  }
+}
+module.exports = AuthRepository;
